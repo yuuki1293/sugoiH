680375e75185d82d39fd407c6a9433ec8fa147f7
yuuki1293 <yuuki1293@outlook.jp>
2022-11-04 18:24:16 +0900

    haskellでは'(シングルクォート)は特別な意味を持たないため関数名に使用できる

--- baby.hs
+++ baby.hs
@@ -4,3 +4,4 @@ doubleSmallNumber x = if x > 100
                         then x
                         else x * 2
 doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+conanO'Brien = "It's a-me, Conan O'Brien!"
