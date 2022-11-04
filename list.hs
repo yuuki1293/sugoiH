5d84e4107d089cc35a279e2982703a3e9b796a73
yuuki1293 <yuuki1293@outlook.jp>
2022-11-04 18:40:35 +0900

    :演算子（cons演算子）でリストの先頭に要素を追加できる

--- list.hs
+++ list.hs
@@ -2,3 +2,5 @@ lostNumbers = [4,8,15,16,23,42]
 longNumbers = [1,2,3,4] ++ [9,10,11,12]
 helloWorld = "hello" ++ " " ++ "world"
 woot = ['w','o'] ++ ['o','t']
+smallCat = 'A':" SMALL CAT"
+numbers = 5:[1,2,3,4,5]
