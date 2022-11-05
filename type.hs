8c9ed8d8abf083ef8ed1542dabbc78d52d35d607
yuuki1293 <yuuki1293@outlook.jp>
2022-11-05 12:31:51 +0900

    Ordは何らかの順序を付けられる型のための型クラス

--- type.hs
+++ type.hs
@@ -3,3 +3,8 @@ eq' = 5/= 5
 eq'' = 'a' == 'a'
 eq''' = "Ho Ho" == "Ho Ho"
 eq'''' = 3.432 == 3.432
+oa = "Abrakadabra" < "Zebra"
+ob = "Abrakadabra" `compare` "Zebra"
+oc = 5 >= 2
+od = 5 `compare` 3
+oe = 'b' > 'a'
