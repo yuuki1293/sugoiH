1355e38b86b2b6694dc7b253eff4b4f011917bdb
yuuki1293 <yuuki1293@outlook.jp>
2022-11-05 10:56:22 +0900

    違う長さのリストを与えると短い方に合わせられる

--- tuple.hs
+++ tuple.hs
@@ -7,3 +7,4 @@ sn = snd (8, 11)
 sn' = snd ("Wow", False)
 zi = zip [1,2,3,4,5] [5,5,5,5,5]
 zi' = zip [1..5] ["one","two","three","four","five"]
+zi'' = zip [5,3,2,6,2,7,2,5,4,6,6] ["im","a","turtle"]
