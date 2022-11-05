131f62bc52437a532a8b024e10545525066f1b10
yuuki1293 <yuuki1293@outlook.jp>
2022-11-05 11:22:19 +0900

    2.2 一般的なHaskellの型 Integer型はIntよりも大きな値を扱える

--- type.hs
+++ type.hs
@@ -2,3 +2,5 @@ removeNonUppercase :: [Char] -> [Char]
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
+factorial :: Integer -> Integer
+factorial n = product [1..n]
