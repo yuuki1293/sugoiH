6be91263e682e72f9db3f9fb1f32413a1823ab55
yuuki1293 <yuuki1293@outlook.jp>
2022-11-05 10:57:20 +0900

    無限のリストと有限のリストをを与えることもできる

--- tuple.hs
+++ tuple.hs
@@ -8,3 +8,4 @@ sn' = snd ("Wow", False)
 zi = zip [1,2,3,4,5] [5,5,5,5,5]
 zi' = zip [1..5] ["one","two","three","four","five"]
 zi'' = zip [5,3,2,6,2,7,2,5,4,6,6] ["im","a","turtle"]
+zi''' = zip [1..] ["apple", "orange", "cherry", "mango"]
