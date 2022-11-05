de559ff61df6c8a19e66ef3238777537568b907e
yuuki1293 <yuuki1293@outlook.jp>
2022-11-05 11:23:52 +0900

    Floatは単精度浮動小数点数

--- type.hs
+++ type.hs
@@ -4,3 +4,5 @@ addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
 factorial :: Integer -> Integer
 factorial n = product [1..n]
+circumference :: Float -> Float
+circumference r = 2 * pi * r
