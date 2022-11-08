dee4b7af563adfb49f66c63099f4e7d2e47cb94d
yuuki1293 <yuuki1293@outlook.jp>
2022-11-08 21:22:26 +0900

    bmiTellからマジックナンバーを消去

--- fun.hs
+++ fun.hs
@@ -1,8 +1,11 @@
 bmiTell :: Double -> Double -> String
 bmiTell weight height
-    | bmi <= 18.5 = "You're underweight, your emo, you!"
-    | bmi <= 25.0 = "You're supposedly normal.\
+    | bmi <= skinny = "You're underweight, your emo, you!"
+    | bmi <= normal = "You're supposedly normal.\
                                     \ Pffft, I bet you're ugly!"
-    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+    | bmi <= fat = "You're fat! Lose some weight, fatty!"
     | otherwise   = "You're a whale, congratulations!"
     where bmi = weight / height ^ 2
+          skinny = 18.5
+          normal = 25.0
+          fat = 30.0
