ae0348951c41a57610a6c26b9c6dfb6810292cc7
yuuki1293 <yuuki1293@outlook.jp>
2022-11-08 21:09:04 +0900

    3.2 場合分けして、きっちりガード！ bmiTell :: Double -> String bmiによってコメントを返す関数

--- fun.hs
+++ fun.hs
@@ -1,46 +1,7 @@
-lucky :: Int -> String
-lucky 7 = "LUCKY NUMBER SEVEN!"
-lucky x = "Sorry, you're out of luck, pal!"
-
-sayMe :: Int -> String
-sayMe 1 = "One!"
-sayMe 2 = "Two!"
-sayMe 3 = "Three!"
-sayMe 4 = "Four!"
-sayMe 5 = "Five!"
-sayMe x = "Not between 1 and 5"
-
-factorial :: Int -> Int
-factorial 0 = 1
-factorial n = n * factorial (n - 1)
-
-charName :: Char -> String
-charName 'a' = "Albert"
-charName 'b' = "Broseph"
-charName 'c' = "Cecil"
-
-addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
-addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
-
-first :: (a, b, c) -> a
-first (x, _, _) = x
-
-second :: (a, b, c) -> b
-second (_, x, _) = x
-
-third :: (a, b, c) -> c
-third (_, _, x) = x
-
-head' :: [a] -> a
-head' [] = error "Can't call head on an empty list, dummy!"
-head' (x:_) = x
-
-tell :: (Show a) => [a] -> String
-tell [] = "The list is empty"
-tell (x:[]) = "The list has one element: " ++ show x
-tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
-tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
-
-firstLetter :: String -> String
-firstLetter "" = "Empty string, whoops!"
-firstLetter all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+bmiTell :: Double -> String
+bmiTell bmi
+  | bmi <= 18.5 = "You're underweight, your emo, you!"
+  | bmi <= 25.0 = "You're supposedly normal.\
+                \ Pffft, I bet you're ugly!"
+  | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+  | otherwise   = "You're a whale, congratulations!"
