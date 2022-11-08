56a88d0e8e72dae396c97516ccefd5e97925703b
yuuki1293 <yuuki1293@outlook.jp>
2022-11-08 19:30:10 +0900

    first :: (a, b, c) -> a second :: (a, b, c) -> b third :: (a, b, c) -> c それぞれタプルの1,2,3番目の要素を取り出す関数

--- fun.hs
+++ fun.hs
@@ -21,3 +21,12 @@ charName 'c' = "Cecil"
 
 addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
 addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, x, _) = x
+
+third :: (a, b, c) -> c
+third (_, _, x) = x
