f9c47e8b6781bec4f58929fe54b24edd51eea530
yuuki1293 <yuuki1293@outlook.jp>
2022-11-08 21:16:32 +0900

    myCompare :: (Ord a) => a -> a -> Ordering 2つの引数を比較する関数

--- fun.hs
+++ fun.hs
@@ -10,3 +10,9 @@ max' :: (Ord a) => a -> a -> a
 max' a b
     | a <= b    = b
     | otherwise = a
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+    | a == b    = EQ
+    | a <= b    = LT
+    | otherwise = GT
