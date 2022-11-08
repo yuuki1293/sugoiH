dd0ead8b05f5125d677b79e6b3704554a5f1b204
yuuki1293 <yuuki1293@outlook.jp>
2022-11-08 21:21:05 +0900

    bmiTell関数の weight / height ^ 2 の部分をwhereによって束縛して名前をつける

--- fun.hs
+++ fun.hs
@@ -1,18 +1,8 @@
 bmiTell :: Double -> Double -> String
-bmiTell weight  height
-    | weight / height ^ 2 <= 18.5 = "You're underweight, your emo, you!"
-    | weight / height ^ 2 <= 25.0 = "You're supposedly normal.\
+bmiTell weight height
+    | bmi <= 18.5 = "You're underweight, your emo, you!"
+    | bmi <= 25.0 = "You're supposedly normal.\
                                     \ Pffft, I bet you're ugly!"
-    | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
     | otherwise   = "You're a whale, congratulations!"
-
-max' :: (Ord a) => a -> a -> a
-max' a b
-    | a <= b    = b
-    | otherwise = a
-
-myCompare :: (Ord a) => a -> a -> Ordering
-a `myCompare` b
-    | a == b    = EQ
-    | a <= b    = LT
-    | otherwise = GT
+    where bmi = weight / height ^ 2
