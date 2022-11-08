44f71b9dba77527ae528a46b1689069d0d0dee5e
yuuki1293 <yuuki1293@outlook.jp>
2022-11-08 21:13:48 +0900

    bmiTellを身長と体重を計算するように変更

--- fun.hs
+++ fun.hs
@@ -1,7 +1,7 @@
-bmiTell :: Double -> String
-bmiTell bmi
-    | bmi <= 18.5 = "You're underweight, your emo, you!"
-    | bmi <= 25.0 = "You're supposedly normal.\
-                  \ Pffft, I bet you're ugly!"
-    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+bmiTell :: Double -> Double -> String
+bmiTell weight  height
+    | weight / height ^ 2 <= 18.5 = "You're underweight, your emo, you!"
+    | weight / height ^ 2 <= 25.0 = "You're supposedly normal.\
+                                    \ Pffft, I bet you're ugly!"
+    | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"
     | otherwise   = "You're a whale, congratulations!"
