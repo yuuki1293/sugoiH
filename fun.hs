69d7dd61cfdd8a009c77a412c75beeef06ba1f3f
yuuki1293 <yuuki1293@outlook.jp>
2022-11-08 21:27:21 +0900

    niceGreeting関数とbadGreeting関数をグローバル関数で束縛

--- fun.hs
+++ fun.hs
@@ -10,9 +10,13 @@ bmiTell weight height
           normal = 25.0
           fat = 30.0
 
+niceGreeting :: String
+niceGreeting = "Hello! So very nice to see you,"
+
+badGreeting :: String
+badGreeting = "Oh! Pfft. It's you."
+
 greet ::String -> String
 greet "Juan" = niceGreeting ++ " Juan!"
 greet "Fernando" = niceGreeting ++ " Fernando!"
 greet name = badGreeting ++ " " ++ name
-    where niceGreeting = "Hello! So very nice to see you,"
-          badGreeting = "Oh! Pfft. It's you."
