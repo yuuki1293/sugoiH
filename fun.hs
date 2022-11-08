7ad29f313a78405a1a77ae5caec2317cd7e54e98
yuuki1293 <yuuki1293@outlook.jp>
2022-11-08 19:44:05 +0900

    firstLetter :: String -> String 文字列と文字列の最初の文字を返す関数

--- fun.hs
+++ fun.hs
@@ -40,3 +40,7 @@ tell [] = "The list is empty"
 tell (x:[]) = "The list has one element: " ++ show x
 tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
 tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+
+firstLetter :: String -> String
+firstLetter "" = "Empty string, whoops!"
+firstLetter all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
