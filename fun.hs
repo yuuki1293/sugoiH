d42b8458d23e498f98671227355e89af4b32b146
yuuki1293 <yuuki1293@outlook.jp>
2022-11-09 14:41:41 +0900

    case式は式の途中でパターンマッチをするのに使える

--- fun.hs
+++ fun.hs
@@ -1,3 +1,9 @@
 head' :: [a] -> a
 head' xs = case xs of [] -> error "No head for empty lists!"
                       (x:_) -> x
+
+describeList :: [a] -> String
+describeList ls = "The list is "
+                  ++ case ls of [] -> "empty."
+                                [x] -> "a singletion list."
+                                xs -> "a longer list."
