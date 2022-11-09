911f10a80217800fdcc77b607ee7ac76d1b11b82
yuuki1293 <yuuki1293@outlook.jp>
2022-11-09 09:13:45 +0900

    3.4 Let It Be let式は変数を束縛できる

--- fun.hs
+++ fun.hs
@@ -1,29 +1,5 @@
-bmiTell :: Double -> Double -> String
-bmiTell weight height
-    | bmi <= skinny = "You're underweight, your emo, you!"
-    | bmi <= normal = "You're supposedly normal.\
-                                    \ Pffft, I bet you're ugly!"
-    | bmi <= fat = "You're fat! Lose some weight, fatty!"
-    | otherwise   = "You're a whale, congratulations!"
-    where bmi = weight / height ^ 2
-          (skinny, normal, fat) = (18.5, 25.0, 30.0)
-
-niceGreeting :: String
-niceGreeting = "Hello! So very nice to see you,"
-
-badGreeting :: String
-badGreeting = "Oh! Pfft. It's you."
-
-greet ::String -> String
-greet "Juan" = niceGreeting ++ " Juan!"
-greet "Fernando" = niceGreeting ++ " Fernando!"
-greet name = badGreeting ++ " " ++ name
-
-initials :: String -> String -> String
-initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
-    where (f:_) = firstname
-          (l:_) = lastname
-
-calcBmis :: [(Double, Double)] -> [Double]
-calcBmis xs = [bmi w h | (w, h) <- xs]
-    where bmi weight height = weight / height ^ 2
+cylinder :: Double -> Double -> Double
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^ 2
+    in sideArea + 2 * topArea
