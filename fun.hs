95be907f3607a2f7f82edca781e626e3bf98afb6
yuuki1293 <yuuki1293@outlook.jp>
2022-11-09 14:50:38 +0900

    4.1 最高に最高！ 再帰関数を使用したリストの最大値を求めるmaximum'関数

--- fun.hs
+++
@@ -1,9 +0,0 @@
-head' :: [a] -> a
-head' xs = case xs of [] -> error "No head for empty lists!"
-                      (x:_) -> x
-
-describeList :: [a] -> String
-describeList ls = "The list is " ++ what ls
-    where what [] = "empty."
-          what [x] = "a singletion list."
-          what xs = "a longer list."
