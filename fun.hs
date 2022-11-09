bb24c5669f970d8e1aae669292dde4e86c148898
yuuki1293 <yuuki1293@outlook.jp>
2022-11-09 14:38:49 +0900

    3.5 case式 パターンマッチはcase式の糖衣構文

--- fun.hs
+++ fun.hs
@@ -1,11 +1,3 @@
-cylinder :: Double -> Double -> Double
-cylinder r h =
-    let sideArea = 2 * pi * r * h
-        topArea = pi * r ^ 2
-    in sideArea + 2 * topArea
-
-le = [let square x = x * x in (square 5, square 3, square 2)]
-le' = (let a = 100; b = 200; c = 300 in a*b*c, let foo = "Hey "; bar = "there!" in foo ++ bar)
-le'' = (let (a, b, c) = (1, 2, 3) in a+b+c) * 100
-calcBmis :: [(Double, Double)] -> [Double]
-calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
+head' :: [a] -> a
+head' xs = case xs of [] -> error "No head for empty lists!"
+                      (x:_) -> x
