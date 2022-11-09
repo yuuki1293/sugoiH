cbba2386e64b3a798bc89848d724d7831379a607
yuuki1293 <yuuki1293@outlook.jp>
2022-11-09 22:07:19 +0900

    5.2 高階実演 applyTwice :: (a -> a) -> a -> a 関数を受け取り、それを2回適用する関数

--- higher.hs
+++ higher.hs
@@ -1,17 +1,2 @@
-multThree :: Int -> (Int -> (Int -> Int))
-multThree x y z = x * y * z
-
-multTwoWithNine :: Int -> (Int -> Int)
-multTwoWithNine = multThree 9
-
-compareWithHundred :: Int -> Ordering
-compareWithHundred x = compare 100 x
-
-compareWithHundred' :: Int -> Ordering
-compareWithHundred' = compare 100
-
-divideByTen :: (Floating  a) => a -> a
-divideByTen = (/10)
-
-isUpperAlphanum :: Char -> Bool
-isUpperAlphanum = (`elem` ['A'..'Z'])
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x) 
