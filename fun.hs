fa976c00f7f47fb21687f75d95ba7af025461d4e
yuuki1293 <yuuki1293@outlook.jp>
2022-11-09 09:25:05 +0900

    リスト内包表記でlet式を使える

--- fun.hs
+++ fun.hs
@@ -7,3 +7,5 @@ cylinder r h =
 le = [let square x = x * x in (square 5, square 3, square 2)]
 le' = (let a = 100; b = 200; c = 300 in a*b*c, let foo = "Hey "; bar = "there!" in foo ++ bar)
 le'' = (let (a, b, c) = (1, 2, 3) in a+b+c) * 100
+calcBmis :: [(Double, Double)] -> [Double]
+calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
