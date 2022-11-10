61ce0a2a4cd77a92d3973d6d3d58eae618b163f7
yuuki1293 <yuuki1293@outlook.jp>
2022-11-10 15:47:39 +0900

    5.3 関数プログラマの道具箱 map' :: (a -> b) -> [a] -> [b] 関数をリストのすべての要素に適用したリストを返す関数

--- higher.hs
+++ higher.hs
@@ -1,10 +1,3 @@
-applyTwice :: (a -> a) -> a -> a
-applyTwice f x = f (f x)
-
-zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipWith' _ [] _ = []
-zipWith' _ _ [] = []
-zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
-
-flip' :: (a -> b -> c) -> b -> a -> c
-flip' f x y = f y x
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
