2ecce43b7ee97308abc7a3affddabc6790b1fd5b
yuuki1293 <yuuki1293@outlook.jp>
2022-11-10 15:51:17 +0900

    filter' :: (a -> Bool) -> [a] -> [a] 述語を満たす要素のみを含むリストを返す関数

--- higher.hs
+++ higher.hs
@@ -1,3 +1,9 @@
 map' :: (a -> b) -> [a] -> [b]
 map' _ [] = []
 map' f (x:xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+    | p x = x : filter' p xs
+    | otherwise = filter' p xs
