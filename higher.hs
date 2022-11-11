56a863007449901bb9874cc196412243c6148f22
yuuki1293 <yuuki1293@outlook.jp>
2022-11-11 13:26:58 +0900

    等価な関数addThreeとaddThree'

--- higher.hs
+++ higher.hs
@@ -13,3 +13,9 @@ ma' = map (\x -> x + 3) [1,6,3,2]
 
 zi = zipWith (\a b -> (a * 30 + 3) / b) [5,4,3,2,1] [1,2,3,4,5]
 ma'' = map (\(a,b) -> a + b) [(1,2),(3,5),(6,3),(2,6),(2,5)]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+addThree' :: Int -> Int -> Int -> Int
+addThree' = \x -> \y -> \z -> x + y + z
