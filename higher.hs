79d4dd3c3a8a45c8d6c2f1ff960e66c2ff9ad989
yuuki1293 <yuuki1293@outlook.jp>
2022-11-11 13:12:04 +0900

    5.4 ラムダ式 ラムダ式を使用したnumLongChains関数

--- higher.hs
+++ higher.hs
@@ -1,28 +1,3 @@
-map' :: (a -> b) -> [a] -> [b]
-map' _ [] = []
-map' f (x:xs) = f x : map' f xs
-
-filter' :: (a -> Bool) -> [a] -> [a]
-filter' _ [] = []
-filter' p (x:xs)
-    | p x = x : filter' p xs
-    | otherwise = filter' p xs
-
-quicksort :: (Ord a) => [a] -> [a]
-quicksort [] = []
-quicksort (x:xs) =
-    let smallerOrEqual = filter (<= x) xs
-        larger = filter (> x) xs
-    in quicksort smallerOrEqual ++ [x] ++ quicksort larger
-
-largestDivisible :: Integer
-largestDivisible = head (filter p [100000, 99999..])
-    where p x = x `mod` 3829 == 0
-
-ta = takeWhile (/=' ') "elephants know how to party"
-
-sqrtUnderthousand = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
-
 chain :: Integer -> [Integer]
 chain 1 = [1]
 chain n
@@ -30,8 +5,5 @@ chain n
     | odd n = n : chain (n * 3 + 1)
 
 numLongChains :: Int
-numLongChains = length (filter isLong (map chain [1..100]))
-    where isLong xs = length xs > 15
-
-listOfFuns :: [Integer -> Integer]
-listOfFuns = map (*) [0..]
+numLongChains = length (filter (\xs -> length xs > 15)
+                               (map chain [1..100]))
