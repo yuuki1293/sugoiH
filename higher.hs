e890b0937d4780be4df530e56a3703c591ec2752
yuuki1293 <yuuki1293@outlook.jp>
2022-11-12 21:46:58 +0900

    foldrを使用したelem'関数

--- higher.hs
+++ higher.hs
@@ -3,3 +3,6 @@ sum' xs = foldl (+) 0 xs
 
 map' :: (a -> b) -> [a] -> [b]
 map' f xs = foldl (\acc x -> acc ++ [f x]) [] xs
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldr (\x acc -> if x == y then True else acc) False ys
