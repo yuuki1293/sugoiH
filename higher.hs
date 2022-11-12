1333b8ffe6d3a922b5a040f50dc628d78fd34039
yuuki1293 <yuuki1293@outlook.jp>
2022-11-12 21:50:30 +0900

    foldl関数を使用したreverse'関数

--- higher.hs
+++ higher.hs
@@ -9,3 +9,6 @@ elem' y ys = foldr (\x acc -> if x == y then True else acc) False ys
 
 maximum' :: (Ord a) => [a] -> a
 maximum' = foldl1 max
+
+reverse' :: [a] -> [a]
+reverse' = foldl (\acc x -> x : acc) []
