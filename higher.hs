5d23bc79e3baad19c50bc57a979d34c82e3e04e4
yuuki1293 <yuuki1293@outlook.jp>
2022-11-12 22:06:08 +0900

    scanl scanr scanl1 scanr1

--- higher.hs
+++ higher.hs
@@ -24,3 +24,8 @@ last' = foldl1 (\_ x -> x)
 
 and' :: [Bool] -> Bool
 and' xs = foldr (&&) True xs
+
+sc = scanl (+) 0 [3,5,2,1]
+sc' = scanr (+) 0 [3,5,2,1]
+sc'' = scanl1 (\acc x -> if x > acc then x else acc) [3,4,5,3,7,9,2,1]
+sc''' = scanl (flip (:)) [] [3,2,1]
