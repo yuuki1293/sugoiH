0bb24b0ff711111a2e34c4175ef6e3e757d81338
yuuki1293 <yuuki1293@outlook.jp>
2022-11-12 21:38:21 +0900

    5.5 畳み込み、見込みアリ！ foldlを使用したsum'関数

--- higher.hs
+++ higher.hs
@@ -1,24 +1,2 @@
-chain :: Integer -> [Integer]
-chain 1 = [1]
-chain n
-    | even n = n : chain (n `div` 2)
-    | odd n = n : chain (n * 3 + 1)
-
-numLongChains :: Int
-numLongChains = length (filter (\xs -> length xs > 15)
-                               (map chain [1..100]))
-
-ma = map (+3) [1,6,3,2]
-ma' = map (\x -> x + 3) [1,6,3,2]
-
-zi = zipWith (\a b -> (a * 30 + 3) / b) [5,4,3,2,1] [1,2,3,4,5]
-ma'' = map (\(a,b) -> a + b) [(1,2),(3,5),(6,3),(2,6),(2,5)]
-
-addThree :: Int -> Int -> Int -> Int
-addThree x y z = x + y + z
-
-addThree' :: Int -> Int -> Int -> Int
-addThree' = \x -> \y -> \z -> x + y + z
-
-flip' :: (a -> b -> c) -> b -> a -> c
-flip' f = \x y -> f y x
+sum' :: (Num a) => [a] -> a
+sum' xs = foldl (\acc x -> acc + x) 0 xs
