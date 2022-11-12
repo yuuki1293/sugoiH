0849c560bec0e8bc02026fb4970390488388003e
yuuki1293 <yuuki1293@outlook.jp>
2022-11-12 21:44:31 +0900

    foldlを使用したmap'関数 ++関数は:よりもはるかに遅いため、リストから新しいリストを構築する際には普通は右畳み込みを使う

--- higher.hs
+++ higher.hs
@@ -2,4 +2,4 @@ sum' :: (Num a) => [a] -> a
 sum' xs = foldl (+) 0 xs
 
 map' :: (a -> b) -> [a] -> [b]
-map' f xs = foldr (\x acc -> f x : acc) [] xs
+map' f xs = foldl (\acc x -> acc ++ [f x]) [] xs
