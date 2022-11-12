e63a7f6065afa64fb2cecfe829b4588156b9a671
yuuki1293 <yuuki1293@outlook.jp>
2022-11-12 21:48:41 +0900

    foldl1関数を使用したmaximum'関数

--- higher.hs
+++ higher.hs
@@ -6,3 +6,6 @@ map' f xs = foldl (\acc x -> acc ++ [f x]) [] xs
 
 elem' :: (Eq a) => a -> [a] -> Bool
 elem' y ys = foldr (\x acc -> if x == y then True else acc) False ys
+
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldl1 max
