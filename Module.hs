bf1b5869702e84c567c43dd4059e6a1c0f023476
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 17:22:57 +0900

    lastName :: Person -> String Person型からラストネームを取り出す関数

--- Module.hs
+++ Module.hs
@@ -3,3 +3,6 @@ data Person = Person String String Int Float String String
 
 firstName :: Person -> String
 firstName (Person firstname _ _ _ _ _) = firstname
+
+lastName :: Person -> String
+lastName (Person _ lastname _ _ _ _) = lastname
