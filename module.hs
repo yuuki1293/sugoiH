9e1e647b0521bf740cd21b4fa47f6c8e64fae5eb
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 13:43:39 +0900

    畳み込みを用いたfindKey関数

--- module.hs
+++ module.hs
@@ -9,6 +9,6 @@ phoneBook =
 
 findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
 findKey key [] = Nothing
-findKey key ((k,v):xs)
-    | key == k  = Just v
-    | otherwise = findKey key xs
+findKey key xs = foldr
+                    (\(k, v) acc -> if key == k then Just v else acc)
+                    Nothing xs
