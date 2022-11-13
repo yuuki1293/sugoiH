4eb7ed339ffe72e504cf5374e1ff7feefbacaa35
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 22:28:58 +0900

    Tree a 型

--- Module.hs
+++ Module.hs
@@ -1,7 +1,1 @@
-infixr 5 :-:
-data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
-
-infixr 5 ^++
-(^++) :: List a -> List a -> List a
-Empty ^++ ys = ys
-(x :-: xs) ^++ ys = x :-: (xs ^++ ys)
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
