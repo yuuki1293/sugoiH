59da53b1771bc304c5b5c88c7b32974860608fc8
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 14:33:13 +0900

    関数を定義

--- Geometry.hs
+++ Geometry.hs
@@ -6,3 +6,24 @@ module Geometry
 , cuboidArea
 , cuboidVolume
 ) where
+
+sphereVolume :: Float -> Float
+sphereVolume radius = (4.0 / 3.0) * pi * (radius ^ 3)
+
+sphereArea :: Float -> Float
+sphereArea radius = 4 * pi * (radius ^ 2)
+
+cubeVolume :: Float -> Float
+cubeVolume side = cuboidVolume side side side
+
+cubeArea :: Float -> Float
+cubeArea side = cuboidArea side side side
+
+cuboidVolume :: Float -> Float -> Float -> Float
+cuboidVolume a b c = reactArea a b * c
+
+cuboidArea :: Float -> Float -> Float -> Float
+cuboidArea a b c = reactArea a b * 2 + reactArea a c * 2 + reactArea c b * 2
+
+reactArea :: Float -> Float -> Float
+reactArea a b = a * b
