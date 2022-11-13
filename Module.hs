df4c2146365075b67d17424cf9a3d0da9337f6d6
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 21:31:15 +0900

    Person型はEq型のインスタンス

--- Module.hs
+++ Module.hs
@@ -1,4 +1,4 @@
 data Person = Person { firstName :: String
                      , lastname :: String
                      , age :: Int
-                     }
+                     } deriving (Eq)
