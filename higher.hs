19cec7f82f8744d7caa537e6c0d5ba030602048b
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 12:10:35 +0900

    laと等価なla'

--- higher.hs
+++ higher.hs
@@ -1,3 +1,4 @@
 maa = map (\x -> negate (abs x)) [5,-3,-6,7,-3,2,-19,24]
 mab = map (negate . abs) [5,-3,-6,7,-3,2,-19,24]
 la = map (\xs -> negate (sum (tail xs))) [[1..5],[3..6],[1..7]]
+la' = map (negate. sum . tail) [[1..5],[3..6],[1..7]]
