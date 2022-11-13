48e4899c50218488e128ceab1416aa88912407db
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 12:56:10 +0900

    文字列を指定した数だけ前方向にシフトする関数

--- module.hs
+++ module.hs
@@ -1,7 +1,4 @@
-import Data.List
+import Data.Char
 
-wordNums :: String -> [(String,Int)]
-wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
-
-isIn :: (Eq a) => [a] -> [a] -> Bool
-needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
+encode :: Int -> String -> String
+encode offset msg = map (\c -> chr $ ord c + offset) msg
