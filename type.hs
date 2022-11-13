c69e719c62f2d482e7940003e2d6fe0071b8597c
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 16:41:19 +0900

    2次元空間の点を表すPoint型

--- type.hs
+++ type.hs
@@ -1,7 +1,2 @@
-data Shape = Circle Float Float Float |
-             Rectangle Float Float Float Float
-    deriving (Show)
-
-area :: Shape -> Float
-area (Circle _ _ r) = pi * r ^ 2
-area (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+data Point = Point Float Float deriving (Show)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
