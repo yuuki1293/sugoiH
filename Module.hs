62fc8c708073795e63ba55818434a0a002cf1e55
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 21:53:46 +0900

    AssocList 多相化された型シノニム

--- Module.hs
+++ Module.hs
@@ -1,16 +1,1 @@
-type PhoneNumber = String
-type Name = String
-type PhoneBook = [(Name, PhoneNumber)]
-
-phoneBook :: PhoneBook
-phoneBook =
-    [("betty", "555-2938")
-    ,("bonnie", "452-2928")
-    ,("patsy", "493-2928")
-    ,("lucille", "205-2928")
-    ,("wendy", "939-8282")
-    ,("penny", "853-2492")
-    ]
-
-inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
-inPhoneBook name pnumber pbook = (name, pnumber) `elem` pbook
+type AssocList k v = [(k, v)]
