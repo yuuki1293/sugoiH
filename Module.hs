043fd0cc00f029c85af92477a26b7b6934a8e61c
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 22:50:53 +0900

    7.9 YesとNoの型クラス YesNo型クラス

--- Module.hs
+++ Module.hs
@@ -1,12 +1,2 @@
-data TrafficLight = Red | Yellow | Green
-
-instance Eq TrafficLight where
-    Red == Red = True
-    Green == Green = True
-    Yellow == Yellow = True
-    _ == _ = False
-
-instance Show TrafficLight where
-    show Red = "Red light"
-    show Yellow = "Yellow light"
-    show Green = "Green light"
+class YesNo a where
+    yesno :: a -> Bool
