33588f2d394d377c691b0be4720097c26b142c73
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 11:59:52 +0900

    5.6 $を使った関数適用 $を使用した等価な関数sua, sub ,suc

--- higher.hs
+++ higher.hs
@@ -1,34 +1,3 @@
-sum' :: (Num a) => [a] -> a
-sum' xs = foldl (+) 0 xs
-
-map' :: (a -> b) -> [a] -> [b]
-map' f xs = foldl (\acc x -> acc ++ [f x]) [] xs
-
-elem' :: (Eq a) => a -> [a] -> Bool
-elem' y ys = foldr (\x acc -> if x == y then True else acc) False ys
-
-maximum' :: (Ord a) => [a] -> a
-maximum' = foldl1 max
-
-reverse' :: [a] -> [a]
-reverse' = foldl (flip (:)) []
-
-product' :: (Num a) => [a] -> a
-product' = foldl (*) 1
-
-filter' :: (a -> Bool) -> [a] -> [a]
-filter' p = foldr (\x acc -> if p x then x : acc else acc) []
-
-last' :: [a] -> a
-last' = foldl1 (\_ x -> x)
-
-and' :: [Bool] -> Bool
-and' xs = foldr (&&) True xs
-
-sc = scanl (+) 0 [3,5,2,1]
-sc' = scanr (+) 0 [3,5,2,1]
-sc'' = scanl1 (\acc x -> if x > acc then x else acc) [3,4,5,3,7,9,2,1]
-sc''' = scanl (flip (:)) [] [3,2,1]
-
-sqrtSums :: Int
-sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
+sua = sum (filter (> 10) (map (*2) [2..10]))
+sub = sum $ filter (> 10) (map (*2) [2..10])
+suc = sum $ filter (>10) $ map (*2) [2..10]
