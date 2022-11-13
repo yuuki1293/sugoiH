6cd4ecabb65eac4cbea8cce462369f11f2073490
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 13:23:05 +0900

    6.3 キーから値へのマッピング 連想リスト

--- module.hs
+++ module.hs
@@ -1,11 +1,8 @@
-import Data.Char
-import Data.List
-
-digitSum :: Int -> Int
-digitSum = sum . map digitToInt . show
-
-firstTo40 :: Maybe Int
-firstTo40 = find (\x -> digitSum x == 40) [1..]
-
-firstTo :: Int -> Maybe Int
-firstTo n = find (\x -> digitSum x == n) [1..]
+phoneBook =
+    [("betty", "555-2938")
+    ,("bonnie", "452-2928")
+    ,("patsy", "493-2928")
+    ,("lucille", "205-2928")
+    ,("wendy", "939-8282")
+    ,("penny", "853-2492")
+    ]
