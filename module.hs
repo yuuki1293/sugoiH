a8dc61a2321496cd96687daa1ced0a4654ffcab7
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 13:39:27 +0900

    安全に使えるfindKey関数

--- module.hs
+++ module.hs
@@ -7,5 +7,8 @@ phoneBook =
     ,("penny", "853-2492")
     ]
 
-findKey :: (Eq k) => k -> [(k, v)] -> v
-findKey key xs = snd . head . filter (\(k, v) -> key == k) $ xs
+findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey key [] = Nothing
+findKey key ((k,v):xs)
+    | key == k  = Just v
+    | otherwise = findKey key xs
