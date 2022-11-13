f4e39882d328efec50256f996ee4983f13281edb
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 13:10:30 +0900

    digitSum :: Int -> Int 各桁の数の和を求める関数

--- module.hs
+++ module.hs
@@ -1,7 +1,5 @@
 import Data.Char
+import Data.List
 
-encode :: Int -> String -> String
-encode offset msg = map (\c -> chr $ ord c + offset) msg
-
-decode :: Int -> String -> String
-decode shift msg = encode (negate shift) msg
+digitSum :: Int -> Int
+digitSum = sum . map digitToInt . show
