2e1972f8b25cfdda7f2f531a9d9382f0bbc45814
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 12:14:51 +0900

    等価な式su, su', su''

--- higher.hs
+++ higher.hs
@@ -2,3 +2,6 @@ maa = map (\x -> negate (abs x)) [5,-3,-6,7,-3,2,-19,24]
 mab = map (negate . abs) [5,-3,-6,7,-3,2,-19,24]
 la = map (\xs -> negate (sum (tail xs))) [[1..5],[3..6],[1..7]]
 la' = map (negate. sum . tail) [[1..5],[3..6],[1..7]]
+su = sum (replicate 5 (max 6.7 8.9))
+su' = (sum . replicate 5) (max 6.7 8.9)
+su'' = sum . replicate 5 $ max 6.7 8.9
