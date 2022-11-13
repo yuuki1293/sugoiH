a0888d1abd0a583ac963059f11b99464e81a323d
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 12:54:02 +0900

    isIn :: (Eq a) => [a] -> [a] -> Bool リストがリストに含まれているか調べる関数

--- module.hs
+++ module.hs
@@ -2,3 +2,6 @@ import Data.List
 
 wordNums :: String -> [(String,Int)]
 wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
+
+isIn :: (Eq a) => [a] -> [a] -> Bool
+needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
