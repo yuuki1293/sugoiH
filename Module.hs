52f4df62e00e23d9fe98875b63487616940f3102
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 17:22:34 +0900

    firstName :: Person -> String Person型からファーストネームを取り出す関数

--- Module.hs
+++ Module.hs
@@ -1,4 +1,5 @@
 data Person = Person String String Int Float String String
     deriving (Show)
 
-buy = Person "Buddy" "Finklestein" 43 184.2 "526-2928" "Chocolate"
+firstName :: Person -> String
+firstName (Person firstname _ _ _ _ _) = firstname
