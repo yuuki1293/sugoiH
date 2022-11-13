1a562a93f891914726b9e0e0d76c8eb39c330630
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 21:45:08 +0900

    7.6 型シノニム [(String ,String)]型のphoneBook関数

--- Module.hs
+++ Module.hs
@@ -1,2 +1,9 @@
-data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
-            deriving (Eq, Ord, Show, Read, Bounded, Enum)
+phoneBook :: [(String ,String)]
+phoneBook =
+    [("betty", "555-2938")
+    ,("bonnie", "452-2928")
+    ,("patsy", "493-2928")
+    ,("lucille", "205-2928")
+    ,("wendy", "939-8282")
+    ,("penny", "853-2492")
+    ]
