ad9daeebe3f325afe5f82a03c4bd140c3ed1361f
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 21:24:50 +0900

    dotProd :: (Num a) => Vector a -> Vector a -> a ２つのベクトルの内積を求める関数

--- Module.hs
+++ Module.hs
@@ -2,3 +2,6 @@ data Vector a = Vector a a a deriving (Show)
 
 vplus :: (Num a) => Vector a -> Vector a -> Vector a
 (Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)
+
+dotProd :: (Num a) => Vector a -> Vector a -> a
+(Vector i j k) `dotProd` (Vector l m n) = i*l + j*m + k*n
