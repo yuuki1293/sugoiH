1afcf9cec1e2b3cb33d53dcfd1a2616d188d1911
yuuki1293 <yuuki1293@outlook.jp>
2022-11-13 12:22:43 +0900

    奇数の平方で10000より小さいものの総和oddSquareSum

--- higher.hs
+++ higher.hs
@@ -13,3 +13,6 @@ sum' = foldl (+) 0
 
 fn x = ceiling (negate (tan (cos (max 50 x))))
 fn' = ceiling . negate . tan . cos . max 50
+
+oddSquareSum :: Integer
+oddSquareSum = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
