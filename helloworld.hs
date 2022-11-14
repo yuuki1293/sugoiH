0c97a112ffc9c827bca05d70b914b2c2adf877aa
yuuki1293 <yuuki1293@outlook.jp>
2022-11-14 10:19:26 +0900

    mapM forMと引数の順序が逆になった関数

--- helloworld.hs
+++ helloworld.hs
@@ -1,7 +1,10 @@
 import Control.Monad
-import Data.Char
 
-main = forever $ do
-    putStr "Give me some input: "
-    l <- getLine
-    putStrLn $ map toUpper l
+main = do
+    colors <- forM [1,2,3,4] $ \a -> do
+        putStrLn $ "Which color do you associate with the number "
+                   ++ show a ++ "?"
+        color <- getLine
+        return color
+    putStrLn "The colors that you associate with 1, 2, 3 and 4 are: "
+    mapM putStrLn colors
