54ea996824d67662af7c86001b3bce111c39867a
yuuki1293 <yuuki1293@outlook.jp>
2022-11-14 09:06:35 +0900

    7.10 Functor型クラス Tree型はFunctor型クラスのインスタンス

--- Module.hs
+++ Module.hs
@@ -1,35 +1,6 @@
-class YesNo a where
-    yesno :: a -> Bool
-
-instance YesNo Int where
-    yesno 0 = False
-    yesno _ = True
-
-instance YesNo [a] where
-    yesno [] = False
-    yesno _ = True
-
-instance YesNo Bool where
-    yesno = id
-
-instance YesNo (Maybe a) where
-    yesno (Just _) = True
-    yesno Nothing = False
-
 data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
 
-instance YesNo (Tree a) where
-    yesno EmptyTree = False
-    yesno _ = True
-
-data TrafficLight = Red | Yellow | Green
-
-instance YesNo TrafficLight where
-    yesno Red = False
-    yesno _ = True
-
-yesnoIf :: (YesNo y) => y -> a -> a -> a
-yesnoIf yesnoVal yesResult noResult =
-    if yesno yesnoVal
-        then yesResult
-        else noResult
+instance Functor Tree where
+    fmap f EmptyTree = EmptyTree
+    fmap f (Node x left right)
+                    = Node (f x) (fmap f left) (fmap f right)
