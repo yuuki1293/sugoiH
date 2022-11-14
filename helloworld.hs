e7db887612685e3f36d74df57896ac541b01d264
yuuki1293 <yuuki1293@outlook.jp>
2022-11-14 09:41:31 +0900

    letで値を束縛

--- helloworld.hs
+++ helloworld.hs
@@ -1,4 +1,12 @@
+import Data.Char
+
 main = do
-    putStrLn "Hello, what's your name?"
-    name <- getLine
-    putStrLn ("Hey " ++ name ++ ", you rock!")
+    putStrLn "What's your first name?"
+    firstName <- getLine
+    putStrLn "What's your last name?"
+    lastName <- getLine
+    let bigFirstName = map toUpper firstName
+        bigLastName = map toUpper lastName
+    putStrLn $ "hey " ++ bigFirstName ++ " "
+                      ++ bigLastName
+                      ++ ", how are you?"
