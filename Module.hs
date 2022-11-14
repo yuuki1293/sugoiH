faf09fcc125fac61198faf84ba37d719e42cbff3
yuuki1293 <yuuki1293@outlook.jp>
2022-11-14 09:25:05 +0900

    hello, worldと表示する

--- Module.hs
+++
@@ -1,6 +0,0 @@
-data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
-
-instance Functor Tree where
-    fmap f EmptyTree = EmptyTree
-    fmap f (Node x left right)
-                    = Node (f x) (fmap f left) (fmap f right)
