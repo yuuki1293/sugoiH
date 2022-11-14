5c39d3dbbc85306b525c7eb8ce2c7a4dad77282d
yuuki1293 <yuuki1293@outlook.jp>
2022-11-14 09:54:28 +0900

    when 条件が満たされた時IOを返す

--- helloworld.hs
+++ helloworld.hs
@@ -1,6 +1,6 @@
+import Control.Monad
+
 main = do
-    print True
-    print 2
-    print "haha"
-    print 3.2
-    print [3,4,3]
+    input <- getLine
+    when (input == "SWORDFISH") $ do
+        putStrLn input
