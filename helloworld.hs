e0ef5b6176f7086ac94f0d1f466d771d8754109b
yuuki1293 <yuuki1293@outlook.jp>
2022-11-14 09:45:06 +0900

    入力された文字列を逆順にして表示する

--- helloworld.hs
+++ helloworld.hs
@@ -1,12 +1,12 @@
 import Data.Char
 
 main = do
-    putStrLn "What's your first name?"
-    firstName <- getLine
-    putStrLn "What's your last name?"
-    lastName <- getLine
-    let bigFirstName = map toUpper firstName
-        bigLastName = map toUpper lastName
-    putStrLn $ "hey " ++ bigFirstName ++ " "
-                      ++ bigLastName
-                      ++ ", how are you?"
+    line <- getLine
+    if null line
+        then return ()
+        else do
+            putStrLn $ reverseWords line
+            main
+
+reverseWords :: String -> String
+reverseWords = unwords . map reverse . words
