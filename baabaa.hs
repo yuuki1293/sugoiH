0a16d389a7d432e53e19727457be55306000f398
yuuki1293 <yuuki1293@outlook.jp>
2022-11-15 10:14:00 +0900

    withFile ラムダ式を受け取り、終了したらファイルハンドルを確実に閉じてくれる関数

--- baabaa.hs
+++ baabaa.hs
@@ -1,7 +1,6 @@
 import System.IO
 
 main = do
-    handle <- openFile "baabaa.txt" ReadMode
-    contents <- hGetContents handle
-    putStr contents
-    hClose handle
+    withFile "baabaa.txt" ReadMode $ \handle -> do
+        contents <- hGetContents handle
+        putStr contents
