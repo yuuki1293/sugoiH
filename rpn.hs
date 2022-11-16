02e6e4cc7f1de76560f5f606c17325090f2ecea2
yuuki1293 <yuuki1293@outlook.jp>
2022-11-16 20:55:39 +0900

    roadStep :: (Path, Path) -> Section -> (Path, Path) Aと Bの最短経路を求める関数

--- rpn.hs
+++ rpn.hs
@@ -9,3 +9,22 @@ heathrowToLondon = [ Section 50 10 30
                    , Section 40 2 25
                    , Section 10 8 0
                    ]
+
+data Label = A | B | C deriving (Show)
+type Path = [(Label, Int)]
+
+roadStep :: (Path, Path) -> Section -> (Path, Path)
+roadStep (pathA, pathB) (Section a b c) =
+    let timeA = sum (map snd pathA)
+        timeB = sum (map snd pathB)
+        forwardTimeToA = timeA + a
+        crossTimeToA = timeB + b + c
+        forwardTimeToB = timeB + b
+        crossTimeToB = timeA + a + c
+        newPathToA = if forwardTimeToA <= crossTimeToA
+                        then (A, a):pathA
+                        else (C, c):(B, b):pathB
+        newPathToB = if forwardTimeToB <= crossTimeToB
+                        then (B, b):pathB
+                        else (C, c):(A, a):pathA
+    in (newPathToA, newPathToB)
