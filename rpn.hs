0a1773a9b2075bffa4669f7a1978b7e4d3a686fb
yuuki1293 <yuuki1293@outlook.jp>
2022-11-16 20:45:05 +0900

    heathrowToLondon 道路網

--- rpn.hs
+++ rpn.hs
@@ -2,3 +2,10 @@ data Section = Section { getA :: Int, getB :: Int, getC :: Int }
     deriving (Show)
 
 type RoadSystem = [Section]
+
+heathrowToLondon :: RoadSystem
+heathrowToLondon = [ Section 50 10 30
+                   , Section 5 90 20
+                   , Section 40 2 25
+                   , Section 10 8 0
+                   ]
