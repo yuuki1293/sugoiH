0f263348c4a4a10741c37bd69c04493aa906fb8f
yuuki1293 <yuuki1293@outlook.jp>
2022-11-16 13:32:21 +0900

    remove :: [String] -> IO () todoリストからアイテムを削除する

--- todo.hs
+++ todo.hs
@@ -2,6 +2,7 @@ import System.Environment
 import System.Directory
 import System.IO
 import Data.List
+import Control.Exception
 
 dispatch :: String -> [String] -> IO ()
 dispatch "add" = add
@@ -22,3 +23,24 @@ view [fileName] = do
         numberedTasks = zipWith (\n line -> show n ++ " - " ++ line)
                     [0..] todoTasks
     putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()
+remove [fileName, numberString] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line)
+                                    [0..] todoTasks
+    putStrLn "These are your TO-DO items:"
+    mapM_ putStrLn numberedTasks
+    let number = read numberString
+        newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
+    bracketOnError (openTempFile "." "temp")
+        (\(tempName, tempHandle) -> do
+            hClose tempHandle
+            removeFile tempName)
+
+        (\(tempName, tempHandle) -> do
+            hPutStr tempHandle newTodoItems
+            hClose tempHandle
+            removeFile fileName
+            renameFile tempName fileName)
