5f5dc8fa262f6dcecdc5320bff7a2fc91b4909fd
yuuki1293 <yuuki1293@outlook.jp>
2022-11-16 20:42:12 +0900

    10.2 ヒースロー空港からロンドンへ Section 3つの道路パーツの所要時間を保持するデータ型

--- rpn.hs
+++ rpn.hs
@@ -1,10 +1,4 @@
-solveRPN :: String -> Double
-solveRPN = head . foldl foldingFunction [] . words
-    where foldingFunction (x:y:ys) "*" = (y * x):ys
-          foldingFunction (x:y:ys) "+" = (y + x):ys
-          foldingFunction (x:y:ys) "-" = (y - x):ys
-          foldingFunction (x:y:ys) "/" = (y / x):ys
-          foldingFunction (x:y:ys) "^" = (y ** x):ys
-          foldingFunction (x:xs) "ln" = log x:xs
-          foldingFunction xs "sum" = [sum xs]
-          foldingFunction xs numberString = read numberString:xs
+data Section = Section { getA :: Int, getB :: Int, getC :: Int }
+    deriving (Show)
+
+type RoadSystem = [Section]
