d8fce7a706b61603179a1cacb6eef4557b7ae45f
yuuki1293 <yuuki1293@outlook.jp>
2022-11-16 13:15:56 +0900

    9.5 ToDoリストをもっと楽しむ dispatch :: String -> [String] -> IO () 文字列によって別々の関数を呼び出す関数

---
+++ todo.hs
@@ -0,0 +1,9 @@
+import System.Environment
+import System.Directory
+import System.IO
+import Data.List
+
+dispatch :: String -> [String] -> IO ()
+dispatch "add" = add
+dispatch "view" = view
+dispatch "remove" = remove
