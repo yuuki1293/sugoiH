f7959a6783f228ff8d4cc68ab050aab7f71e9e6d
yuuki1293 <yuuki1293@outlook.jp>
2022-11-17 21:10:53 +0900

    11.3 アプリカティブファンクターを使おう myAction :: IO String 文字列を2つ読み取り連結して返す関数

--- cmaybe.hs
+++ cmaybe.hs
@@ -1,5 +1,5 @@
-data CMaybe a = CNothing | CJust Int a deriving (Show)
-
-instance Functor CMaybe where
-    fmap f CNothing = CNothing
-    fmap f (CJust counter x) = CJust (counter+1) (f x)
+myAction :: IO String
+myAction = do
+    a <- getLine
+    b <- getLine
+    return $ a ++ b
