bee04666426e4285ae2b0da138d8f0d1f8d9e972
yuuki1293 <yuuki1293@outlook.jp>
2022-11-19 21:53:38 +0900

    Orderingモノイドを使用したlengthCompare関数

--- monoid.hs
+++ monoid.hs
@@ -1,4 +1,5 @@
+import Data.Monoid
+
 lengthCompare :: String -> String -> Ordering
-lengthCompare x y = let a = length x `compare` length y
-                        b = x `compare` y
-                    in  if a == EQ then b else a
+lengthCompare x y = (length x `compare` length y) `mappend`
+                    (x `compare` y)
