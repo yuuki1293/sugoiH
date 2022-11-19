79ed11042500f5ac5e7b6e73a93bba6ecf12de3a
yuuki1293 <yuuki1293@outlook.jp>
2022-11-19 21:51:51 +0900

    12.2 モノイドとの遭遇 lengthCompare :: String -> String -> Ordering 文字列の長さを比較し、もし同じなら辞書順で比較する関数

--- monoid.hs
+++ monoid.hs
@@ -1,4 +1,4 @@
-newtype CoolBool = CoolBool { getCoolBool :: Bool }
-
-helloMe :: CoolBool -> String
-helloMe (CoolBool _) = "hello"
+lengthCompare :: String -> String -> Ordering
+lengthCompare x y = let a = length x `compare` length y
+                        b = x `compare` y
+                    in  if a == EQ then b else a
