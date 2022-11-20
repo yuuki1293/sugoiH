e17c034c6a0609e724e7e0356fca0a5129aa1951
yuuki1293 <yuuki1293@outlook.jp>
2022-11-20 21:32:14 +0900

    TreeはFoldableのインスタンス

--- monoid.hs
+++ monoid.hs
@@ -1,1 +1,9 @@
+import qualified Data.Foldable as F
+
 data Tree a = EmptyTree | Node a (Tree a) (Tree b) deriving (Show)
+
+instance F.Foldable Tree where
+    foldMap f EmptyTree = mempty
+    foldMap f (Node x l r) = F.foldMap f l `mappend`
+                             f x           `mappend`
+                             F.foldMap f r
