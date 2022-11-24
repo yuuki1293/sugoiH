416c1919da8cb0cfc1a8944b6e9aeec222951635
yuuki1293 <yuuki1293@outlook.jp>
2022-11-24 18:01:55 +0900

    13.2 Maybeから始めるモナド applyMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b Maybe aの値とa->Maybe bの関数からMaybe bを返す関数

--- monoid.hs
+++
@@ -1,9 +0,0 @@
-import qualified Data.Foldable as F
-
-data Tree a = EmptyTree | Node a (Tree a) (Tree b) deriving (Show)
-
-instance F.Foldable Tree where
-    foldMap f EmptyTree = mempty
-    foldMap f (Node x l r) = F.foldMap f l `mappend`
-                             f x           `mappend`
-                             F.foldMap f r
