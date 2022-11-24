1c75770c61d3f9807c8b33e30007705a7fa4e714
yuuki1293 <yuuki1293@outlook.jp>
2022-11-24 18:29:14 +0900

    landLeftとlandRight関数を改良

--- monad.hs
+++ monad.hs
@@ -1,8 +1,12 @@
 type Birds = Int
 type Pole = (Birds, Birds)
 
-landLeft :: Birds -> Pole -> Pole
-landLeft n (left, right) = (left + n, right)
+landLeft :: Birds -> Pole -> Maybe Pole
+landLeft n (left, right)
+    | abs ((left + n) - right) < 4 = Just (left + n, right)
+    | otherwise                    = Nothing
 
-landRight :: Birds -> Pole -> Pole
-landRight n (left, right) = (left, right + n)
+landRight :: Birds -> Pole -> Maybe Pole
+landRight n (left, right)
+    | abs (left - (right + n)) < 4 = Just (left, right + n)
+    | otherwise                    = Nothing
