ae9480966e304e7862ecb5d67c262bafe2906e16
yuuki1293 <yuuki1293@outlook.jp>
2022-11-24 18:18:11 +0900

    landRight :: Birds -> Pole -> Pole 右の鳥を増やす関数

--- monad.hs
+++ monad.hs
@@ -3,3 +3,6 @@ type Pole = (Birds, Birds)
 
 landLeft :: Birds -> Pole -> Pole
 landLeft n (left, right) = (left + n, right)
+
+landRight :: Birds -> Pole -> Pole
+landRight n (left, right) = (left, right + n)
