4d128dbcd4781bcf860179ae00ad271ebbe83674
yuuki1293 <yuuki1293@outlook.jp>
2022-11-25 23:32:53 +0900

    13.5 do記法 Myabeの値を繋げて返す

--- monad.hs
+++ monad.hs
@@ -1,15 +1,4 @@
-type Birds = Int
-type Pole = (Birds, Birds)
-
-landLeft :: Birds -> Pole -> Maybe Pole
-landLeft n (left, right)
-    | abs ((left + n) - right) < 4 = Just (left + n, right)
-    | otherwise                    = Nothing
-
-landRight :: Birds -> Pole -> Maybe Pole
-landRight n (left, right)
-    | abs (left - (right + n)) < 4 = Just (left, right + n)
-    | otherwise                    = Nothing
-
-banana :: Pole -> Maybe Pole
-banana _ = Nothing
+foo :: Maybe String
+foo = Just 3   >>= (\x ->
+      Just "!" >>= (\y ->
+      Just (show x ++ y)))
