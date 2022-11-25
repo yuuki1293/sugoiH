5d3e4ac23715b36cdf90a28ce972df0d94630273
yuuki1293 <yuuki1293@outlook.jp>
2022-11-26 00:15:01 +0900

    moveKnight :: KnightPos -> [KnightPos] 動けるマスを列挙する関数

--- monad.hs
+++ monad.hs
@@ -1,1 +1,11 @@
+import Control.Monad
+
 type KnightPos = (Int, Int)
+
+moveKnight :: KnightPos -> [KnightPos]
+moveKnight (c,r) = do
+    (c',r') <- [(c+2,r-1),(c+2,r+1),(c-2,r-1),(c-2,r+1)
+              ,(c+1,r-2),(c+1,r+2),(c-1,r-2),(c-1,r+2)
+              ]
+    guard (c' `elem` [1..8] && r' `elem` [1..8])
+    return (c', r')
