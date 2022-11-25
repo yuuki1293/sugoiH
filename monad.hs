6a94ef55671dabaf5c0e6198df707b57cea077af
yuuki1293 <yuuki1293@outlook.jp>
2022-11-26 00:49:39 +0900

    endにたどり着くまでの経路を知れるように改良

--- monad.hs
+++ monad.hs
@@ -2,16 +2,17 @@ import Control.Monad
 
 type KnightPos = (Int, Int)
 
-moveKnight :: KnightPos -> [KnightPos]
-moveKnight (c,r) = do
+moveKnight :: [KnightPos] -> [[KnightPos]]
+moveKnight ((c,r):xs) = do
     (c',r') <- [(c+2,r-1),(c+2,r+1),(c-2,r-1),(c-2,r+1)
               ,(c+1,r-2),(c+1,r+2),(c-1,r-2),(c-1,r+2)
               ]
     guard (c' `elem` [1..8] && r' `elem` [1..8])
-    return (c', r')
+    return ((c', r'):(c,r):xs)
 
-in3 :: KnightPos -> [KnightPos]
-in3 start = return start >>= moveKnight >>= moveKnight >>= moveKnight
+in3 :: KnightPos -> [[KnightPos]]
+in3 start = return [start] >>= moveKnight >>= moveKnight >>= moveKnight
 
-canRearchIn3 :: KnightPos -> KnightPos -> Bool
-canRearchIn3 start end = end `elem` in3 start
+canRearchIn3 :: KnightPos -> KnightPos -> [[KnightPos]]
+canRearchIn3 start end = 
+    filter (\(x:xs) -> end == x) (in3 start)
