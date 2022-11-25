5bd0f1bc235ae9a1da52d35b52b5f1be9e76e777
yuuki1293 <yuuki1293@outlook.jp>
2022-11-25 23:37:57 +0900

    一連のMaybeの処理

--- monad.hs
+++ monad.hs
@@ -1,5 +1,22 @@
-foo :: Maybe String
-foo = do
-    x <- Just 3
-    y <- Just "!"
-    Just (show x ++ y)
+type Birds = Int
+type Pole = (Birds, Birds)
+
+landLeft :: Birds -> Pole -> Maybe Pole
+landLeft n (left, right)
+    | abs ((left + n) - right) < 4 = Just (left + n, right)
+    | otherwise                    = Nothing
+
+landRight :: Birds -> Pole -> Maybe Pole
+landRight n (left, right)
+    | abs (left - (right + n)) < 4 = Just (left, right + n)
+    | otherwise                    = Nothing
+
+banana :: Pole -> Maybe Pole
+banana _ = Nothing
+
+routine :: Maybe Pole
+routine = do
+    start <- return (0, 0)
+    first <- landLeft 2 start
+    second <- landRight 2 first
+    landLeft 1 second
