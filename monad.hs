15f00814a00fce23486a1b553187fb9b576f895d
yuuki1293 <yuuki1293@outlook.jp>
2022-11-26 18:28:08 +0900

    logにリスト以外のモノイドでも使えるように

--- monad.hs
+++ monad.hs
@@ -1,5 +1,5 @@
 isBigGang :: Int -> (Bool, String)
 isBigGang x = (x > 9, "Compared gang size to 9.")
 
-applyLog :: (a, [c]) -> (a -> (b, [c])) -> (b, [c])
-applyLog (x, log) f = let (y, newLog) = f x in (y, log ++ newLog)
+applyLog :: (Monoid m) => (a, m) -> (a -> (b, m)) -> (b, m)
+applyLog (x, log) f = let (y, newLog) = f x in (y, log `mappend` newLog)
