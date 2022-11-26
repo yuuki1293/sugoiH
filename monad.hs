64696c53fa9466a39955e4b33d5597f083232a8b
yuuki1293 <yuuki1293@outlook.jp>
2022-11-26 18:26:01 +0900

    より抽象化したapplyLog

--- monad.hs
+++ monad.hs
@@ -1,5 +1,5 @@
 isBigGang :: Int -> (Bool, String)
 isBigGang x = (x > 9, "Compared gang size to 9.")
 
-applyLog :: (a, String) -> (a -> (b, String)) -> (b, String)
+applyLog :: (a, [c]) -> (a -> (b, [c])) -> (b, [c])
 applyLog (x, log) f = let (y, newLog) = f x in (y, log ++ newLog)
