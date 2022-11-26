7d3bffc6117acc944d0ae160da139ac9897893d7
yuuki1293 <yuuki1293@outlook.jp>
2022-11-26 18:33:43 +0900

    addDrink :: Food -> (Food, Price) 食事の注文を取ってドリンクと価格を返す関数

--- monad.hs
+++ monad.hs
@@ -1,5 +1,9 @@
-isBigGang :: Int -> (Bool, String)
-isBigGang x = (x > 9, "Compared gang size to 9.")
+import Data.Monoid
 
-applyLog :: (Monoid m) => (a, m) -> (a -> (b, m)) -> (b, m)
-applyLog (x, log) f = let (y, newLog) = f x in (y, log `mappend` newLog)
+type Food = String
+type Price = Sum Int
+
+addDrink :: Food -> (Food, Price)
+addDrink "beans" = ("milk", Sum 25)
+addDrink "jerky" = ("whiskey", Sum 99)
+addDrink _ = ("beer", Sum 30)
