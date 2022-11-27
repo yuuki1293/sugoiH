bbd79e3ca15144001dbaa66b3f748c5b5a514dd3
yuuki1293 <yuuki1293@outlook.jp>
2022-11-27 12:45:41 +0900

    DiffListを使用したgcd7関数

--- monad.hs
+++ monad.hs
@@ -1,3 +1,5 @@
+import Data.Monoid
+import Data.Semigroup
 import Control.Monad.Writer
 
 newtype DiffList a = DiffList { getDiffList :: [a] -> [a]}
@@ -5,9 +7,21 @@ newtype DiffList a = DiffList { getDiffList :: [a] -> [a]}
 toDiffList :: [a] -> DiffList a
 toDiffList xs = DiffList (xs++)
 
-formDiffList :: DiffList a -> [a]
-formDiffList (DiffList f) = f []
+fromDiffList :: DiffList a -> [a]
+fromDiffList (DiffList f) = f []
+
+instance Semigroup (DiffList a) where
+    (DiffList f) <> (DiffList g) = DiffList (\xs -> f (g xs))
 
 instance Monoid (DiffList a) where
     mempty = DiffList (\xs -> [] ++ xs)
-    (DiffList f) `mappend` (DiffList g) = DiffList (\xs -> f (g xs))
+
+gcd' :: Int -> Int -> Writer (DiffList String) Int
+gcd' a b
+    | b == 0 = do
+        tell (toDiffList ["Finished with " ++ show a])
+        return a
+    | otherwise = do
+        result <- gcd' b (a `mod` b)
+        tell (toDiffList [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)])
+        return result
