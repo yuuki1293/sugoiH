79ba0c1f9f8a21ec9ba6004c89d385220721532e
yuuki1293 <yuuki1293@outlook.jp>
2022-11-27 12:20:59 +0900

    Difflist 差分リスト

--- monad.hs
+++ monad.hs
@@ -1,20 +1,3 @@
 import Control.Monad.Writer
 
-gcd' :: Int -> Int -> Writer [String] Int
-gcd' a b
-    | b == 0 = do
-        tell ["Finished with " ++ show a]
-        return a
-    | otherwise = do
-        tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
-        gcd' b (a `mod` b)
-
-gcdReverse :: Int -> Int -> Writer [String] Int
-gcdReverse a b
-    | b == 0 = do
-        tell ["Finished with " ++ show a]
-        return a
-    | otherwise = do
-        result <- gcdReverse b (a `mod` b)
-        tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
-        return result
+newtype Difflist a = DiffList { getDiffList :: [a] -> [a]}
