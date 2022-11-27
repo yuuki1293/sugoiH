372f3d9ee64cf1f26a4c12fb31a15e3370aad676
yuuki1293 <yuuki1293@outlook.jp>
2022-11-27 12:18:19 +0900

    効率の悪いgcdReverse関数

--- monad.hs
+++ monad.hs
@@ -8,3 +8,13 @@ gcd' a b
     | otherwise = do
         tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
         gcd' b (a `mod` b)
+
+gcdReverse :: Int -> Int -> Writer [String] Int
+gcdReverse a b
+    | b == 0 = do
+        tell ["Finished with " ++ show a]
+        return a
+    | otherwise = do
+        result <- gcdReverse b (a `mod` b)
+        tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+        return result
