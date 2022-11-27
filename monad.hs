13a70877a5a2f5a18717cb07f814b3826bff14c6
yuuki1293 <yuuki1293@outlook.jp>
2022-11-27 12:49:10 +0900

    finalCountDown :: Int -> Writer (DiffList String) () カウントダウンする関数

--- monad.hs
+++ monad.hs
@@ -16,12 +16,9 @@ instance Semigroup (DiffList a) where
 instance Monoid (DiffList a) where
     mempty = DiffList (\xs -> [] ++ xs)
 
-gcd' :: Int -> Int -> Writer (DiffList String) Int
-gcd' a b
-    | b == 0 = do
-        tell (toDiffList ["Finished with " ++ show a])
-        return a
-    | otherwise = do
-        result <- gcd' b (a `mod` b)
-        tell (toDiffList [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)])
-        return result
+finalCountDown :: Int -> Writer (DiffList String) ()
+finalCountDown 0 = do
+    tell (toDiffList ["0"])
+finalCountDown x = do
+    finalCountDown (x-1)
+    tell (toDiffList [show x])
