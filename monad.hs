53a130cee88c0be7c3dd96f747911489512df554
yuuki1293 <yuuki1293@outlook.jp>
2022-11-27 13:08:35 +0900

    14.2 Reader？ それはあなたです！ addStuff :: Int -> Int 関数モナドを使うdo式

--- monad.hs
+++ monad.hs
@@ -1,24 +1,7 @@
-import Data.Monoid
-import Data.Semigroup
-import Control.Monad.Writer
+import Control.Monad.Instances
 
-newtype DiffList a = DiffList { getDiffList :: [a] -> [a]}
-
-toDiffList :: [a] -> DiffList a
-toDiffList xs = DiffList (xs++)
-
-fromDiffList :: DiffList a -> [a]
-fromDiffList (DiffList f) = f []
-
-instance Semigroup (DiffList a) where
-    (DiffList f) <> (DiffList g) = DiffList (\xs -> f (g xs))
-
-instance Monoid (DiffList a) where
-    mempty = DiffList (\xs -> [] ++ xs)
-
-finalCountDown :: Int -> Writer [String] ()
-finalCountDown 0 = do
-    tell ["0"]
-finalCountDown x = do
-    finalCountDown (x-1)
-    tell [show x]
+addStuff :: Int -> Int
+addStuff = do
+    a <- (*2)
+    b <- (+10)
+    return (a+b)
