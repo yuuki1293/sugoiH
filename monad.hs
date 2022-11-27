e302c8841b587ee14811a3de9c540ff7587e1795
yuuki1293 <yuuki1293@outlook.jp>
2022-11-27 13:10:32 +0900

    わかりやすくしたaddStuff関数

--- monad.hs
+++ monad.hs
@@ -1,7 +1,7 @@
 import Control.Monad.Instances
 
 addStuff :: Int -> Int
-addStuff = do
-    a <- (*2)
-    b <- (+10)
-    return (a+b)
+addStuff x = let
+    a = (*2) x
+    b = (+10) x
+    in a+b
