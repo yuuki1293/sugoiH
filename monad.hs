fa06e96dd203f17477daa8c94914d20667d168f4
yuuki1293 <yuuki1293@outlook.jp>
2022-11-27 12:15:26 +0900

    ログを取りながら最大公約数を求める関数

--- monad.hs
+++ monad.hs
@@ -1,4 +1,10 @@
-gcd' :: Int -> Int -> Int
+import Control.Monad.Writer
+
+gcd' :: Int -> Int -> Writer [String] Int
 gcd' a b
-    | b == 0    = a
-    | otherwise = gcd' b (a `mod` b)
+    | b == 0 = do
+        tell ["Finished with " ++ show a]
+        return a
+    | otherwise = do
+        tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+        gcd' b (a `mod` b)
