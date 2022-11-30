a99a570fe89ae9b01f3945a306abcda67dc29ef8
yuuki1293 <yuuki1293@outlook.jp>
2022-11-30 12:51:05 +0900

    stackManip :: Stack -> (Int, Stack) stack操作をシミュレート

--- monad.hs
+++ monad.hs
@@ -5,3 +5,9 @@ pop (x:xs) = (x, xs)
 
 push :: Int -> Stack -> ((), Stack)
 push a xs = ((), a:xs)
+
+stackManip :: Stack -> (Int, Stack)
+stackManip stack = let
+    ((), newStack1) = push 3 stack
+    (a, newStack2) = pop newStack1
+    in pop newStack2
