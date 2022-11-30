28220d1ea1f24a5dacac9f0db8bf1eedb588bfff
yuuki1293 <yuuki1293@outlook.jp>
2022-11-30 16:58:16 +0900

    binSmalls :: Int -> Int -> Maybe Int 第2引数が9以下なら加算が成功する関数

--- monad.hs
+++ monad.hs
@@ -1,14 +1,7 @@
 import Control.Monad.Trans.Writer
 import Control.Monad
 
-keepSmall :: Int -> Writer [String] Bool
-keepSmall x
-    | x < 4 = do
-        tell ["Keeping " ++ show x]
-        return True
-    | otherwise = do
-        tell [show x ++ " is too large, throwing it away"]
-        return False
-
-powerset:: [a] -> [[a]]
-powerset xs = filterM (\x -> [True, False]) xs
+binSmalls :: Int -> Int -> Maybe Int
+binSmalls acc x
+    | x > 9 = Nothing
+    | otherwise = Just (acc + x)
