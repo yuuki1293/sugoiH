12f61cae668210b308d8abb457302268416c282b
yuuki1293 <yuuki1293@outlook.jp>
2022-11-30 15:33:22 +0900

    3枚のコインを投げる

--- monad.hs
+++ monad.hs
@@ -3,3 +3,10 @@ import Control.Monad.Trans.State
 
 randomSt :: (RandomGen g, Random a) => State g a
 randomSt = state random
+
+threeCoins ::  State StdGen (Bool, Bool, Bool)
+threeCoins = do
+    a <- randomSt
+    b <- randomSt
+    c <- randomSt
+    return (a, b, c)
