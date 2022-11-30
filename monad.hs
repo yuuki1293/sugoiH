354a15a58f23300b815337c4db16e12b68b3d1bf
yuuki1293 <yuuki1293@outlook.jp>
2022-11-30 21:16:48 +0900

    14.6 安全な逆ポーランド記法電卓を作ろう readMaybe :: (Read a) => String -> Maybe a 読み取りに成功したらJust aを返す関数

--- monad.hs
+++ monad.hs
@@ -1,4 +1,14 @@
-binSmalls :: Int -> Int -> Maybe Int
-binSmalls acc x
-    | x > 9 = Nothing
-    | otherwise = Just (acc + x)
+import Data.List
+
+solveRPN :: String -> Double
+solveRPN = head . foldl foldingFunction [] . words
+
+foldingFunction :: [Double] -> String -> Maybe [Double]
+foldingFunction (x:y:ys) "*" = (y * x):ys
+foldingFunction (x:y:ys) "+" = (y + x):ys
+foldingFunction (x:y:ys) "-" = (y - x):ys
+foldingFunction xs numberString = read numberString:xs
+
+readMaybe :: (Read a) => String -> Maybe a
+readMaybe st = case reads st of [(x, "")] -> Just x
+                                _ -> Nothing
