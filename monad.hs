5e2e7d31c37f2eac3a63d40ac7bd566909e0bb5f
yuuki1293 <yuuki1293@outlook.jp>
2022-11-30 21:20:22 +0900

    foldingFunctionがMaybeを返すように

--- monad.hs
+++ monad.hs
@@ -1,13 +1,14 @@
 import Data.List
+import Control.Monad
 
 solveRPN :: String -> Double
 solveRPN = head . foldl foldingFunction [] . words
 
 foldingFunction :: [Double] -> String -> Maybe [Double]
-foldingFunction (x:y:ys) "*" = (y * x):ys
-foldingFunction (x:y:ys) "+" = (y + x):ys
-foldingFunction (x:y:ys) "-" = (y - x):ys
-foldingFunction xs numberString = read numberString:xs
+foldingFunction (x:y:ys) "*" = return ((y * x):ys)
+foldingFunction (x:y:ys) "+" = return ((y + x):ys)
+foldingFunction (x:y:ys) "-" = return ((y - x):ys)
+foldingFunction xs numberString = liftM (:xs) (readMaybe numberString)
 
 readMaybe :: (Read a) => String -> Maybe a
 readMaybe st = case reads st of [(x, "")] -> Just x
