a6620cb56f22a875dbcf7711825147cbcb5e9207
yuuki1293 <yuuki1293@outlook.jp>
2022-11-30 14:04:07 +0900

    randomSt :: (RandomGen g, Random a) => State g a 乱数を発生させる状態モナド

--- monad.hs
+++ monad.hs
@@ -1,30 +1,5 @@
+import System.Random
 import Control.Monad.Trans.State
-type Stack = [Int]
 
-pop :: State Stack Int
-pop = state $ \(x:xs) -> (x, xs)
-
-push :: Int -> State Stack ()
-push a = state $ \xs -> ((), a:xs)
-
-stackManip :: State Stack Int
-stackManip = do
-    push 3
-    a <- pop
-    pop
-
-stackStuff :: State Stack ()
-stackStuff = do
-    a <- pop
-    if a == 5
-        then push 5
-        else do
-            push 3
-            push 8
-
-moreStack :: State Stack ()
-moreStack = do
-    a <- stackManip
-    if a == 100
-        then stackStuff
-        else return ()
+randomSt :: (RandomGen g, Random a) => State g a
+randomSt = state random
