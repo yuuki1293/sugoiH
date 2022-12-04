7a5701da805bc1e7e344d489edc6e54db8bdde8b
yuuki1293 <yuuki1293@outlook.jp>
2022-12-05 00:04:58 +0900

    サンプルシチュエーション

--- monad.hs
+++ monad.hs
@@ -4,3 +4,9 @@ newtype Prob a = Prob { getProb :: [(a, Rational)] } deriving Show
 
 instance Functor Prob where
     fmap f (Prob xs) = Prob $ map (\(x, p) -> (f x, p)) xs
+
+thisSituation :: Prob (Prob Char)
+thisSituation = Prob
+    [(Prob [('a',1%2),('b',1%2)], 1%4)
+    ,(Prob [('c',1%2),('d',1%2)], 3%4)
+    ]
