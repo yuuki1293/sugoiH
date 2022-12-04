37847d5438249b5dcd8a6f5856b704450c3c198c
yuuki1293 <yuuki1293@outlook.jp>
2022-12-04 23:59:55 +0900

    14.8 モナドを作る aと分数のタプルのリストProb型

--- monad.hs
+++ monad.hs
@@ -1,17 +1,3 @@
-import Data.List
-import Control.Monad
+import Data.Ratio
 
-solveRPN :: String -> Maybe Double
-solveRPN st = do
-    [result] <- foldM foldingFunction [] (words st)
-    return result
-
-foldingFunction :: [Double] -> String -> Maybe [Double]
-foldingFunction (x:y:ys) "*" = return ((y * x):ys)
-foldingFunction (x:y:ys) "+" = return ((y + x):ys)
-foldingFunction (x:y:ys) "-" = return ((y - x):ys)
-foldingFunction xs numberString = liftM (:xs) (readMaybe numberString)
-
-readMaybe :: (Read a) => String -> Maybe a
-readMaybe st = case reads st of [(x, "")] -> Just x
-                                _ -> Nothing
+newtype Prob a = Prob { getProb :: [(a, Rational)] } deriving Show
