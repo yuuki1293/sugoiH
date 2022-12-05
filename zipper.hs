69a7a856c293f082467ce02172b07720961fe932
yuuki1293 <yuuki1293@outlook.jp>
2022-12-05 23:06:35 +0900

    FSZipper型シノニム

--- zipper.hs
+++ zipper.hs
@@ -2,3 +2,4 @@ type Name = String
 type Data = String
 data FSItem = File Name Data | Folder Name [FSItem] deriving (Show)
 data FSCrumb = FSCrumb Name [FSItem] [FSItem] deriving (Show)
+type FSZipper = (FSItem, [FSCrumb])
