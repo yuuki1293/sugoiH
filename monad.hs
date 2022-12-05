38852cc2fc790fd0cf720f9cfa0f49d11047d24e
yuuki1293 <yuuki1293@outlook.jp>
2022-12-05 19:58:56 +0900

    シミュレーション

--- monad.hs
+++ monad.hs
@@ -1,8 +1,8 @@
 import Data.Ratio
-import Control.Monad
 import System.Directory.Internal.Prelude (Applicative)
 import Control.Monad.Except (Functor)
 import Control.Monad.RWS.Strict (MonadFail)
+import Data.List
 
 newtype Prob a = Prob { getProb :: [(a, Rational)] } deriving Show
 
@@ -35,3 +35,13 @@ data Coin = Heads | Tails deriving (Show, Eq)
 
 coin :: Prob Coin
 coin = Prob [(Heads, 1%2),(Tails,1%2)]
+
+loadedCoin :: Prob Coin
+loadedCoin = Prob [(Heads,1%10),(Tails,9%10)]
+
+flipThree :: Prob Bool
+flipThree = do
+    a <- coin
+    b <- coin
+    c <- loadedCoin
+    return (all (==Tails) [a,b,c])
