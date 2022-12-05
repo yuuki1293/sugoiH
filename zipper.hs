d481084405c0cd94913faa4c560676066fce7b69
yuuki1293 <yuuki1293@outlook.jp>
2022-12-05 23:39:16 +0900

    15.4 足下にご注意

--- zipper.hs
+++ zipper.hs
@@ -1,50 +1,52 @@
-import Data.List (break)
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
+data Crumb a = LeftCrumb a (Tree a)
+             | RightCrumb a (Tree a) deriving (Show)
+type Breadcrumbs a = [Crumb a]
+type Zipper a = (Tree a, Breadcrumbs a)
 
-type Name = String
-type Data = String
-data FSItem = File Name Data | Folder Name [FSItem] deriving (Show)
-data FSCrumb = FSCrumb Name [FSItem] [FSItem] deriving (Show)
-type FSZipper = (FSItem, [FSCrumb])
+goLeft :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
+goLeft (Node x l r, bs) = (l, LeftCrumb x r:bs)
 
-fsUp :: FSZipper -> FSZipper
-fsUp (item, FSCrumb name ls rs:bs) =
-    (Folder name (ls ++ [item] ++ rs), bs)
+goRight :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
+goRight (Node x l r, bs) = (r, RightCrumb x l:bs)
 
-fsTo :: Name -> FSZipper -> FSZipper
-fsTo name (Folder folderName items, bs) =
-    let (ls, item:rs) = break (nameIs name) items
-    in  (item, FSCrumb folderName ls rs:bs)
+goUp :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
+goUp (t, LeftCrumb x r:bs) = (Node x t r, bs)
+goUp (t, RightCrumb x l:bs) = (Node x l t, bs)
 
-nameIs :: Name -> FSItem -> Bool
-nameIs name (Folder folderName _) = name == folderName
-nameIs name (File fileName _) = name == fileName
+modify :: (a -> a) -> Zipper a -> Zipper a
+modify f (Node x l r, bs) = (Node (f x) l r, bs)
+modify f (Empty, bs) = (Empty, bs)
 
-fsRename :: Name -> FSZipper -> FSZipper
-fsRename newName (Folder name items, bs) = (Folder newName items, bs)
-fsRename newName (File name dat, bs) = (File newName dat, bs)
+attach :: Tree a -> Zipper a -> Zipper a
+attach t (_, bs) = (t, bs)
 
-fsNewFile :: FSItem -> FSZipper -> FSZipper
-fsNewFile item (Folder folderName items, bs) =
-    (Folder folderName (item:items), bs)
+topMost :: Zipper a -> Zipper a
+topMost (t, []) = (t, [])
+topMost z = topMost (goUp z)
 
-myDisk :: FSItem
-myDisk =
-    Folder "root"
-        [ File "goat_yelling_like_man.wmv" "baaaaaa"
-        , File "pope_time.avi" "god bless"
-        , Folder "pics"
-            [ File "ape_throwing_up.jpg" "bleargh"
-            , File "watermelon_smash.gif" "smash!!"
-            , File "skull_man(scary).bmp" "Yikes!"
-            ]
-        , File "dijon_poupon.doc" "best mustard"
-        , Folder "programs"
-            [ File "fartwizard.exe" "10gotofart"
-            , File "owl_bandit.dmg" "mov eax, h00t"
-            , File "not_a_virus.exe" "really not a virus"
-            , Folder "source code"
-                [ File "best_hs_prog.hs" "main = print (fix error)"
-                , File "random.hs" "main = print 4"
-                ]
-            ]
-        ]
+x -: f = f x
+
+freeTree :: Tree Char
+freeTree =
+    Node 'P'
+        (Node 'O'
+            (Node 'L'
+                (Node 'N' Empty Empty)
+                (Node 'T' Empty Empty)
+            )
+            (Node 'Y'
+                (Node 'S' Empty Empty)
+                (Node 'A' Empty Empty)
+            )
+        )
+        (Node 'L'
+            (Node 'W'
+                (Node 'C' Empty Empty)
+                (Node 'R' Empty Empty)
+            )
+            (Node 'A'
+                (Node 'A' Empty Empty)
+                (Node 'C' Empty Empty)
+            )
+        )
