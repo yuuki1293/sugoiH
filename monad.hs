95185a3cb73ce5081cf651855a7338424a2d5c46
yuuki1293 <yuuki1293@outlook.jp>
2022-12-05 20:33:17 +0900

    elemAt :: Directions -> Tree a -> a 目的地にある要素を返す関数

--- monad.hs
+++ monad.hs
@@ -31,3 +31,8 @@ changeToP :: Directions -> Tree Char -> Tree Char
 changeToP [] (Node _ l r) = Node 'P' l r
 changeToP (L:ds) (Node x l r) = Node x (changeToP ds l) r
 cahngeToP (R:ds) (Node x l r) = Node x l (changeToP ds r)
+
+elemAt :: Directions -> Tree a -> a
+elemAt (L:ds) (Node _ l _) = elemAt ds l
+elemAt (R:ds) (Node _ _ r) = elemAt ds r
+elemAt [] (Node x _ _) = x
