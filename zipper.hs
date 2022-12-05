5f24368d420178c75a6e3eb9cf36c09cebcad41b
yuuki1293 <yuuki1293@outlook.jp>
2022-12-05 23:41:49 +0900

    Maybeの文脈をつけたgoLeft, goRight関数

--- zipper.hs
+++ zipper.hs
@@ -4,11 +4,13 @@ data Crumb a = LeftCrumb a (Tree a)
 type Breadcrumbs a = [Crumb a]
 type Zipper a = (Tree a, Breadcrumbs a)
 
-goLeft :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
-goLeft (Node x l r, bs) = (l, LeftCrumb x r:bs)
+goLeft :: Zipper a -> Maybe (Zipper a)
+goLeft (Node x l r, bs) = Just (l, LeftCrumb x r:bs)
+goLeft (Empty, _) = Nothing
 
-goRight :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
-goRight (Node x l r, bs) = (r, RightCrumb x l:bs)
+goRight :: Zipper a -> Maybe (Zipper a)
+goRight (Node x l r, bs) = Just (r, RightCrumb x l:bs)
+goRight (Empty, _) = Nothing
 
 goUp :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
 goUp (t, LeftCrumb x r:bs) = (Node x t r, bs)
