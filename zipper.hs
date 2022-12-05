f1df26baff6f1df92b090049035e8e80e2be922a
yuuki1293 <yuuki1293@outlook.jp>
2022-12-05 23:09:14 +0900

    fsUp :: FSZipper -> FSZipper 親ファイルに移動する関数

--- zipper.hs
+++ zipper.hs
@@ -3,3 +3,7 @@ type Data = String
 data FSItem = File Name Data | Folder Name [FSItem] deriving (Show)
 data FSCrumb = FSCrumb Name [FSItem] [FSItem] deriving (Show)
 type FSZipper = (FSItem, [FSCrumb])
+
+fsUp :: FSZipper -> FSZipper
+fsUp (item, FSCrumb name ls rs:bs) =
+    (Folder name (ls ++ [item] ++ rs), bs)
