3c04b0e57d2d546d726d58919eb64faf26f9ff2c
yuuki1293 <yuuki1293@outlook.jp>
2022-12-05 20:56:43 +0900

    goLeftとgoRightを更新

--- monad.hs
+++ monad.hs
@@ -42,10 +42,10 @@ data Crumb a = LeftCrumb a (Tree a)
 
 type Breadcrumbs a = [Crumb a]
 
-goLeft :: (Tree a, Breadcrumbs) -> (Tree a, Breadcrumbs)
-goLeft (Node _ l _, bs) = (l, L:bs)
+goLeft :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
+goLeft (Node x l r, bs) = (l, LeftCrumb x r:bs)
 
-goRight :: (Tree a, Breadcrumbs) -> (Tree a, Breadcrumbs)
-goRight (Node _ _ r, bs) = (r, R:bs)
+goRight :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
+goRight (Node x l r, bs) = (r, RightCrumb x l:bs)
 
 x -: f = f x
